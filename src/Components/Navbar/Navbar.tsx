790065d1c52ce5c4daf44399d47ef44a48995687
Mekafukame <grzegorz.romanczuk.99@gmail.com>
2022-09-04 21:02:02 +0200

    set navbar height

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,16 +4,17 @@ import { SxProps, StackProps } from "@mui/system";
 export type NavbarProps = SxProps & StackProps & {};
 
 const defaultProps: NavbarProps = {
-  height: { xs: 56, md: 48 },
+  height: { xs: 48 },
+  minHeight: { xs: 48 },
 };
 
 export const Navbar: React.FC<NavbarProps> = (props) => {
-  const { height } = defaultProps;
+  const { height, minHeight } = defaultProps;
 
   return (
     <React.Fragment>
       <AppBar position="sticky" sx={{ height }}>
-        <Toolbar></Toolbar>
+        <Toolbar sx={{ height: "100%", minHeight }}></Toolbar>
       </AppBar>
     </React.Fragment>
   );
